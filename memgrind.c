62fbefe16d7c8b62ed37889c45996f91eae6c475
Oluwademilade Alao <oaa100@assembly.cs.rutgers.edu>
2025-10-01 19:44:47 -0400

    done v1

--- memgrind.c
+++ memgrind.c
@@ -6,7 +6,11 @@
 #include <string.h>
 #include "mymalloc.h"
 
-#define SSIZE 5;
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
 
 int main()
 {
@@ -24,7 +28,7 @@ int main()
     int choice = 0;
     int allocations = 0;
     int idx = 0;
-    while (flag < 2)
+    while (flag < 50)
     {
         for (int i = 0; i < 120; i++)
         {
@@ -36,7 +40,6 @@ int main()
             }
             free(p);
         }
-
         for (int i = 0; i < 120; i++)
         {
             objs[i] = malloc(1);
@@ -65,7 +68,6 @@ int main()
                 }
             }
         }
-
         for (int i = 0; i < 120; i++)
         {
             header *p = (header *)ptrs[idx];
@@ -75,11 +77,70 @@ int main()
                 free(p);
             }
         }
+
+        Node *head = NULL;
+        head = (Node *)malloc(sizeof(Node));
+        head->data = 1;
+        int f = 0;
+
+        Node *cur = head;
+
+        while (f < 85)
+        {
+            Node *n = (Node *)malloc(sizeof(Node));
+            n->next = NULL;
+            cur->next = n;
+            cur = cur->next;
+            f++;
+        }
+        f = 0;
+        cur = head;
+        while (cur != NULL)
+        {
+            Node *prev = cur;
+            cur = cur->next;
+            free(prev);
+            f++;
+        }
+
+        Node *head2 = NULL;
+        head2 = (Node *)malloc(sizeof(Node));
+        head2->data = 1;
+        Node *cur2 = head;
+        int f2 = 0;
+        while (f2 < 85)
+        {
+            Node *n2 = (Node *)malloc(sizeof(Node));
+            n2->next = NULL;
+            cur2->next = n2;
+            cur2 = cur2->next;
+            f2++;
+        }
+        Node *prev2 = head;
+        Node *node = head->next;
+
+        while (prev2 != NULL && node != NULL)
+        {
+            prev2->next = node->next;
+            free(node);
+            prev2 = prev2->next;
+            if (prev2 != NULL)
+                node = prev2->next;
+        }
+        node = head;
+        f2 = 0;
+        while (node != NULL)
+        {
+            Node *prev2 = node;
+            node = node->next;
+            free(prev2);
+            f2++;
+        }
         flag++;
     }
 
     gettimeofday(&t1, NULL);
-    long elapsed = (t1.tv_usec - t0.tv_usec) / 120;
+    long elapsed = (t1.tv_usec - t0.tv_usec) / 50;
     printf("Time elapsed: %ld microseconds\n", elapsed);
     return 0;
 }
